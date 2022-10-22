2926787a3688beb228b947d33929919676025c5e
Ayham <ayham.alazm@gmail.com>
2022-10-22 15:57:34 +0300

    add guards for creating, updating Category and Item models, stage migration files

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,14 +23,39 @@ class Category extends Model
         return $this->hasMany(Item::class, 'category_id');
     }
 
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
     // -------------------- Methods -------------------
     public function canContainSubs()
     {
-        return !$this->subs()->exists();
+        return !$this->items()->exists();
     }
 
     public function canContainItems()
     {
-        return !$this->items()->exists();
+        return !$this->subs()->exists();
+    }
+
+    // -------------------- Overrides -------------------
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($item) {
+            $parent = $item->parent;
+            if ($parent && !$parent->canContainSubs()) {
+                abort(400, 'Sorry, this parent can\'t contain subs');
+            }
+        });
+
+        static::updating(function ($item) {
+            $parent = $item->parent;
+            if ($parent && !$parent->canContainSubs()) {
+                abort(400, 'Sorry, this parent can\'t contain subs');
+            }
+        });
     }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -9,6 +9,31 @@ class Item extends Model
 {
     use HasFactory;
 
-    public $fillable = ['name', 'image','category_id'];
+    public $fillable = ['name', 'image', 'category_id'];
 
+    // -------------------- Relationships -------------------
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    // -------------------- Overrides -------------------
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($item) {
+            $category = $item->category;
+            if ($category && !$category->canContainItems()) {
+                abort(400, 'Sorry, this category can\'t contain items');
+            }
+        });
+
+        static::updating(function ($item) {
+            $category = $item->category;
+            if ($category && !$category->canContainItems()) {
+                abort(400, 'Sorry, this category can\'t contain items');
+            }
+        });
+    }
 }
